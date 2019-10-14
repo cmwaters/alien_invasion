7f0e7f269ef69712fb5024e22cf600bccaed806b
Callum Michael Waters <c.waters@campus.tu-berlin.de>
2019-10-14 10:29:29 +0200

    reworked and cleaned up the main test

--- main_test.go
+++ main_test.go
@@ -1,47 +1,55 @@
 package main
 
 import (
-	"github.com/cmwaters/alien_invasion/internal/city"
+	a "github.com/cmwaters/alien_invasion/internal/alien"
+	c "github.com/cmwaters/alien_invasion/internal/city"
 	"testing"
 )
 
 func TestAlienMovesFromOneCityToAnother(t *testing.T) {
-	firstCity := city.City{Name: "firstCity"}
-	secondCity := city.City{Name: "secondCity"}
-	thirdCity := city.City{Name: "thirdCity"}
-	firstCity.Aliens = append(firstCity.Aliens, 7)
+	cities = make(map[string]*c.City)
+	aliens = make(map[int]*a.Alien)
+	cities["firstCity"] = &c.City{Name: "firstCity"}
+	cities["secondCity"] = &c.City{Name: "secondCity"}
+	cities["thirdCity"] = &c.City{Name: "thirdCity"}
+	aliens[7] = &a.Alien{
+		Id:   7,
+		City: cities["firstCity"].Name,
+	}
+	cities["firstCity"].Aliens = make(map[int]*a.Alien)
+	cities["firstCity"].Aliens[7] = aliens[7]
 	// function should not move an alien if an alien has no city to go to
-	oldCity, newCity := moveAlien(7, firstCity)
-	if newCity.Name != oldCity.Name || len(newCity.Aliens) != 1 {
-		t.Errorf("Alien has failed to stay trapped in its city. Result %s : %s", newCity.Name, newCity.aliensToString())
+	moveAlien(*aliens[7])
+	if aliens[7].City != cities["firstCity"].Name || len(cities["firstCity"].Aliens) != 1 {
+		t.Errorf("Alien has failed to stay trapped in its city. Result: \n" + simulationStatus())
 	}
 	// function should move alien from first city to second city
-	firstCity.West = &secondCity
-	secondCity.East = &firstCity
-	oldCity, newCity = moveAlien(7, firstCity)
-	if len(oldCity.Aliens) != 0 || newCity.Aliens[0] != 7 {
+	cities["firstCity"].West = cities["secondCity"]
+	cities["secondCity"].East = cities["firstCity"]
+	moveAlien(*aliens[7])
+	if len(cities["firstCity"].Aliens) != 0 || len(cities["secondCity"].Aliens) != 1 {
 		t.Errorf("Failed to Move alien. There should be 1 alien in secondCity. There is %d. There should be 0 "+
-			"aliens in firstCity. There is %d.", len(newCity.Aliens), len(oldCity.Aliens))
-	}
-	// function should return the original city if the alien is not in the city (an error is also produced)
-	oldCity, newCity = moveAlien(13, secondCity)
-	if oldCity.Name != secondCity.Name || newCity.Name != secondCity.Name {
-		t.Errorf("Alien which doesn't exist has moved")
+			"aliens in firstCity. There is %d.", len(cities["secondCity"].Aliens), len(cities["firstCity"].Aliens))
 	}
 	// function to check that multiple aliens can move across to one city
-	thirdCity.North = &firstCity
-	firstCity.South = &thirdCity
-	thirdCity.Aliens = append(thirdCity.Aliens, 11)
-	thirdCity.Aliens = append(thirdCity.Aliens, 4)
-	oldCity, newCity = moveAlien(11, thirdCity)
-	thirdCity = oldCity
-	firstCity = newCity
-	oldCity, newCity = moveAlien(4, thirdCity)
-	thirdCity = oldCity
-	firstCity = newCity
-	if len(thirdCity.Aliens) != 0 || len(firstCity.Aliens) != 3 {
-		t.Errorf("Aliens haven't moved succesfully across from third city to first city. Respective Cities: %s;"+
-			" %s", thirdCity.aliensToString(), firstCity.aliensToString())
+	cities["thirdCity"].North = cities["secondCity"]
+	cities["secondCity"].South = cities["thirdCity"]
+	aliens[11] = &a.Alien{
+		Id:   11,
+		City: "thirdCity",
+	}
+	aliens[4] = &a.Alien{
+		Id:   4,
+		City: "thirdCity",
+	}
+	cities["thirdCity"].Aliens = make(map[int]*a.Alien)
+	cities["thirdCity"].Aliens[11] = aliens[11]
+	cities["thirdCity"].Aliens[4] = aliens[4]
+	moveAlien(*aliens[11])
+	moveAlien(*aliens[4])
+	if len(cities["thirdCity"].Aliens) != 0 || len(cities["secondCity"].Aliens) != 3 {
+		t.Errorf("Aliens haven't moved succesfully across from third city to second city. Status: \n" +
+			simulationStatus())
 	}
 
 }
